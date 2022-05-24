942e2ad3bdf5f413030ff56c299d4b3ec9e4092a
Erin Weed <erinllynch@gmail.com>
2022-05-23 21:50:24 -0600

    Pass tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let forwards = [...str.toLowerCase()]
+  let forwardsLetters = forwards.filter((item)=>{
+    let ascii = item.charCodeAt()
+    return ascii >= 97 && ascii <= 122})
+  let backwards = forwardsLetters.slice().reverse()
+  return forwardsLetters.toString() == backwards.toString()
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
