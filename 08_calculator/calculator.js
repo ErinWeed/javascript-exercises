b600850590a4af2be41037c4116e46ab9f52755a
Erin Weed <erinllynch@gmail.com>
2022-05-23 21:04:30 -0600

    Update with all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let answer =  arr.reduce((total, current)=> {
+    return total + current}, 0);
+  return answer
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, current)=>{return product * current},1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  return n <= 1 ? 1 : n * factorial(n-1)
 };
 
 // Do not edit below this line
